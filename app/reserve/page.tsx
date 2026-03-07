14257b0177171e2df3bdafd41d531e2ab2fb4cf7
sineru38 <sineru38@gmail.com>
2026-03-07 09:25:08 +0900

    Add dynamic export to reserve page

--- app/reserve/page.tsx
+++ app/reserve/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic' // 이 줄을 추가하세요!
 import { useState, useEffect } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import Link from 'next/link'
